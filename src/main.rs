97b2b3859bbf49c69b57ba72af31c01302f111c4
CrashOverride <daxorinator@gmail.com>
2020-01-05 23:13:42 +0000

    Using Luminance now

--- src/main.rs
+++ src/main.rs
@@ -4,31 +4,18 @@ use winit::{
     window::WindowBuilder,
 };
 
-fn main() {
-    let event_loop = EventLoop::new();
-    let window = WindowBuilder::new().build(&event_loop).unwrap();
-    
-    event_loop.run(move |event, _, control_flow| {
-        match event {
-            Event::MainEventsCleared => {
-                window.request_redraw();
-            },
-            Event::RedrawRequested(_) => {
-                // Redraw the application.
-                //
-                // It's preferrable to render in this event rather than in MainEventsCleared, since
-                // rendering in here allows the program to gracefully handle redraws requested
-                // by the OS.
-            },
-            Event::WindowEvent {
-                event: WindowEvent::CloseRequested,
-                ..
-            } => {
-                println!("The close button was pressed; stopping");
-                *control_flow = ControlFlow::Exit
-            },
+use luminance_glutin::{
+    GlutinSurface,
+    Surface as _,
+    WindowDim, WindowOpt
+};
 
-            _ => *control_flow = ControlFlow::Poll,
-        }
-    });
+fn main() {
+
+    let surface = GlutinSurface::new(
+        WindowDim::Windowed(640, 480), 
+        "Testing",
+        WindowOpt::default()
+    );
+    
 }
